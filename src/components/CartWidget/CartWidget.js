81c325a6c6336e882e909abb886a2edecb10de69
Matias Martinez <matimartinezz.927@gmail.com>
2022-11-08 15:03:35 -0300

    PreEntrega1 Martinez

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,13 @@
+import "./style.css";
+import { BsCart2 } from 'react-icons/bs';
+
+const CartWidget = () => {
+	return(
+		<button className="CartWidget">
+			<BsCart2/>
+		</button>
+		
+	)
+}
+
+export default CartWidget;
---
+++ src/components/ItenListContainer/ItenListContainer.js
@@ -0,0 +1,8 @@
+import "./style.css"
+
+const ItemListContainer = ({greeting}) => {
+	return ( <p className="text"> { greeting ="Bienvenido a Mateando Ando"} </p>
+	);	
+};
+
+export default ItemListContainer;
