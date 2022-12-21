4a9a3c9707936083f7582531206f4b719605fc68
Matias Martinez <matimartinezz.927@gmail.com>
2022-12-21 18:48:22 -0300

    Proyecto Final

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import { router } from "./router/index";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0MvISA4mqIvRHbc0fQ1pr0ZBteGqQX04",
-  authDomain: "ecommerce-mateando-ando.firebaseapp.com",
-  projectId: "ecommerce-mateando-ando",
-  storageBucket: "ecommerce-mateando-ando.appspot.com",
-  messagingSenderId: "404438655623",
-  appId: "1:404438655623:web:c0a3d6dde091cd2f150e22"
+	apiKey: process.env.REACT_APP_API_KEY,
+  	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  	projectId: process.env.REACT_APP_PROJECT_ID,
+  	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  	appId: process.env.REACT_APP_APP_ID
 };
 
 
